d390abbc19b03481a7d45ff3768f7547baaacc67
Verma <ashwani.verma@adidas.com>
2021-12-11 17:24:37 +0530

    added 404 page

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, r.URL.Path)
 	} else if r.URL.Path == "/Contact" {
 		fmt.Fprintf(w, "To get in touch send mail to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>We could not find the page you are looking for</h1><p>Please email us if you keep being sent to an invalid page</p>")
 	}
 }
 
