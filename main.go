5ed43defd4a304ddaa8671b94463201bff59ea79
Verma <ashwani.verma@adidas.com>
2021-12-11 17:07:19 +0530

    added contact page

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, r.URL.Path)
+	} else if r.URL.Path == "/Contact" {
+		fmt.Fprintf(w, "To get in touch send mail to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
 }
 
 func main() {
