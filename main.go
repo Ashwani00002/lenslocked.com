11b13d41535738c48121ce32b3613fb461f3c33c
Verma <ashwani.verma@adidas.com>
2021-12-12 14:45:18 +0530

    custom 404 page

--- main.go
+++ main.go
@@ -18,10 +18,23 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "To get in touch send mail to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h2>You have reached the faq section</h2>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "<h3>Sorry, the page you are looking for does not exist </h3>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/faq", faq)
 
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
