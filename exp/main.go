1d5575424314210fd2f323244811759ca9ff5ab7
Verma <ashwani.verma@adidas.com>
2022-06-18 23:42:01 +0530

    063-Creating records and logging with GORM

--- exp/main.go
+++ exp/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const (
@@ -20,11 +24,14 @@ type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+	Color string
 }
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -36,7 +43,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	db.AutoMigrate(&User{})
+	name, email, color := getInfo()
 
+	u := User{
+		Name:  name,
+		Email: email,
+		Color: color,
+	}
+
+	fmt.Printf("%+v\n", u)
+}
+
+func getInfo() (name, email, color string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+	fmt.Println("What is your favourite color?")
+	color, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	email = strings.TrimSpace(email)
+	color = strings.TrimSpace(color)
+	return name, email, color
 }
